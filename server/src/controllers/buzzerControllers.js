3a1fbbb2b40ef679b5e806c7af60993f6e03161d
ENOSH RODRIGO <111069060+enoshrodrigo@users.noreply.github.com>
2024-11-28 18:32:49 +0530

    fixed the bug,  its called wrong join query

--- server/src/controllers/buzzerControllers.js
+++ server/src/controllers/buzzerControllers.js
@@ -17,13 +17,13 @@ const buzzerStart = async(req,res)=>{
     }
 
  const buzzerClick = async(req,res)=>{
-    console.log(req.query.buzzerId)
+   // console.log(req.query.buzzerId)
     const io = await socket.getIO();
          db.query('INSERT INTO buzzer (buzzerID)VALUES (?) ',req.query.buzzerId,async(err,res)=>{
-           err?console.log('err'):''
+           //err?console.log(err):''
              if(res){
-              db.query('SELECT * FROM buzzer JOIN team_assign ON buzzer.id = team_assign.buzzer_id;',(err,result)=>{
-                console.log(result)
+              db.query('SELECT * FROM buzzer JOIN team_assign ON buzzer.buzzerID = team_assign.buzzer_id;',(err,result)=>{
+               // console.log(result)
                 err?console.log('buzzer seelct'):''  
                 io.emit("buzzerNUmber", {
                   buzzerOrder: result, 
@@ -41,18 +41,24 @@ const buzzerReset = async(req,res)=>{
     //const {time, isGameOnline, massege, question} = req.body;
     //check buzzer round online 
     db.query('SELECT is_online FROM status  WHERE game_id = 21003',(err,result)=>{
-      err?console.log('err'):''  
+      err?console.log(err):''  
       if(result[0].is_online == 1){
         db.query('TRUNCATE TABLE buzzer',async(err,res)=>{
+
         //  err?console.log('err'):''
             if(res){
+              //tempory code
+              io.emit("buzzerNUmber", {
+                buzzerOrder: [], 
+              });
              console.log('table truncated')
            
      db.query('SELECT * FROM buzzer_questions WHERE is_displayed = 0 LIMIT 1',(err,result)=>{
-          err?console.log(err):''  
+         // err?console.log(err):''  
           if(result){
+            console.log(result)
             db.query('UPDATE buzzer_questions SET is_displayed = 1 WHERE id = ?',result[0]?.id,(err,res)=>{
-              err?console.log('err'):''  
+              //err?console.log('err'):''  
               if(res){
                 console.log('question displayed')
                 io.emit("buzzerStart", {
